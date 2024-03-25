b7bc13a34c32dcae70be465b0cdaea3e307e92ea
MertOzmen <avmertozmen93@gmail.com>
2024-03-25 16:36:33 +0300

    Class ortamı düzenlendi ve US402 de işlemler yapıldı.

--- src/OpenMRS_Project/POM_Mert.java
+++ src/OpenMRS_Project/POM_Mert.java
@@ -1,8 +1,16 @@
 package OpenMRS_Project;
 
 import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class POM_Mert {
     public POM_Mert(){PageFactory.initElements(BaseDriver.driver, this);}
+
+    @FindBy (css = "[class='zak-header-button zak-header-button--1']")
+    public WebElement demoButton;
+
+
+
 }
