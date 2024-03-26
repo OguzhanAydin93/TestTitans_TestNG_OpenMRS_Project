d57bb5fe12b42d81145c48c37f0236d164d436fb
MertOzmen <avmertozmen93@gmail.com>
2024-03-26 14:20:58 +0300

    Düzenlemeler

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -36,8 +36,7 @@ public class BaseDriver {
         //break;
         //default:driver=new ChromeDriver();break;
 
-
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
