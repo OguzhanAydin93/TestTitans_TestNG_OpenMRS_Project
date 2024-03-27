8089e10bd70526adab03e4d0235a8b9a438e8f29
MertOzmen <avmertozmen93@gmail.com>
2024-03-27 15:52:05 +0300

    Değişiklikler

--- src/OpenMRS_Project/OpenMRS.java
+++ src/OpenMRS_Project/OpenMRS.java
@@ -4,7 +4,6 @@ import Utility.BaseDriver;
 import Utility.MyFunc;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
@@ -24,11 +23,14 @@ public class OpenMRS extends BaseDriver {
         elementler.demo.click();
 
         wait.until(ExpectedConditions.elementToBeClickable(elementler.exploreOpenMRS2));
+        elementler.js.executeScript("arguments[0].scrollIntoView(true);", elementler.exploreOpenMRS2);
         elementler.exploreOpenMRS2.click();
 
         wait.until(ExpectedConditions.elementToBeClickable(elementler.enterOpenMRS2Demo));
-        MyFunc.bekle(1);
-        elementler.enterOpenMRS2Demo.click();
+        elementler.js.executeScript("arguments[0].scrollIntoView(true);", elementler.enterOpenMRS2Demo);
+        elementler.js.executeScript("arguments[0].click();", elementler.enterOpenMRS2Demo);
+
+
 
         wait.until(ExpectedConditions.visibilityOf(elementler.username));
         elementler.username.sendKeys(username);
@@ -69,10 +71,11 @@ public class OpenMRS extends BaseDriver {
 
         wait.until(ExpectedConditions.elementToBeClickable(elements.demoButton));
         elements.demoButton.click();
+        elements.js.executeScript("arguments[0].scrollIntoView(true);", elements.exploreButton);
         wait.until(ExpectedConditions.elementToBeClickable(elements.exploreButton));
         elements.exploreButton.click();
-        MyFunc.bekle(2);
-        elements.enterMrsButton.click();
+        elements.js.executeScript("arguments[0].scrollIntoView(true);", elements.enterMrsButton);
+        elements.js.executeScript("arguments[0].click();", elements.enterMrsButton);
         wait.until(ExpectedConditions.visibilityOf(elements.userName));
         elements.userName.sendKeys(userName);
         wait.until(ExpectedConditions.visibilityOf(elements.password));
@@ -98,6 +101,7 @@ public class OpenMRS extends BaseDriver {
 
         }
     }
+
     @Test
     public void US_403_Zehra() {
         Zehra_POM elements = new Zehra_POM();
@@ -109,14 +113,15 @@ public class OpenMRS extends BaseDriver {
         wait.until(ExpectedConditions.urlContains("login"));
 
     }
+
     @Test
-    public void US_404_Nuri(){
+    public void US_404_Nuri() {
 
     }
 
     @Test
-    public void US_405_Zehra(){
-        Zehra_POM elements=new Zehra_POM();
+    public void US_405_Zehra() {
+        Zehra_POM elements = new Zehra_POM();
         elements.login();
 
         new Actions(driver).moveToElement(elements.admin).build().perform();
@@ -127,7 +132,7 @@ public class OpenMRS extends BaseDriver {
 
         for (WebElement dogrula : elements.passwordLanguages) {
             System.out.println(dogrula.getText());
-            Assert.assertTrue(dogrula.isDisplayed(),"Change password ve My Languages bulunamadı.");
+            Assert.assertTrue(dogrula.isDisplayed(), "Change password ve My Languages bulunamadı.");
         }
 
         elements.changePassword.click();
@@ -138,10 +143,11 @@ public class OpenMRS extends BaseDriver {
         driver.navigate().back();
 
     }
+
     @Test(dataProvider = "deletedPatient")
-    public void US_407_Zehra(String patientDeleted){
-        Zehra_POM elements=new Zehra_POM();
-        Actions actionDriver=new Actions(driver);
+    public void US_407_Zehra(String patientDeleted) {
+        Zehra_POM elements = new Zehra_POM();
+        Actions actionDriver = new Actions(driver);
         elements.login();
 
 
@@ -158,12 +164,10 @@ public class OpenMRS extends BaseDriver {
 
         wait.until(ExpectedConditions.urlContains("findPatient"));
         elements.patientSearch.sendKeys(patientDeleted + Keys.ENTER);
-        Assert.assertTrue(elements.deleteConfirm.isDisplayed(),"Hasta silinemedi.");
+        Assert.assertTrue(elements.deleteConfirm.isDisplayed(), "Hasta silinemedi.");
 
 
     }
-
-
     @DataProvider
     Object[][] notSuccessfully() {
         Object[][] usernameAndPasswordd =
@@ -179,10 +183,17 @@ public class OpenMRS extends BaseDriver {
         return usernameAndPasswordd;
     }
 
+
+
+
     @DataProvider
-    Object[] deletedPatient(){
-        Object[] dltdPatient={"zehra"};
+    Object[] deletedPatient() {
+        Object[] dltdPatient = {"zehra"};
         return dltdPatient;
 
+    }
+    public void US_408_Mert_Oguzhan(){
+        POM_Mert elements = new POM_Mert();
+        elements.Login();
     }
 }
--- src/OpenMRS_Project/POM_Mert.java
+++ src/OpenMRS_Project/POM_Mert.java
@@ -62,4 +62,5 @@ public class POM_Mert {
 
 
 
+
 }
