99661682936fe73c8f17dedcdb5d43a659357f0d
ZGK <zehragkrl@gmail.com>
2024-04-04 20:47:06 +0000

    İsteğe bağlı US 411  tamamlandı.

--- src/OpenMRS_Project/OpenMRS.java
+++ src/OpenMRS_Project/OpenMRS.java
@@ -356,6 +356,14 @@ public class OpenMRS extends BaseDriver {
         Zehra_POM elements = new Zehra_POM();
         elements.login();
 
+        elements.appoinment.click();
+        elements.manageAppoinment.click();
+        wait.until(ExpectedConditions.urlContains("findPatient"));
+        elements.appoinmentSearch.sendKeys("zehra");
+        wait.until(ExpectedConditions.visibilityOf(elements.patientInfo));
+        elements.patientInfo.click();
+        wait.until(ExpectedConditions.urlContains("appointmentschedulingui"));
+        Assert.assertFalse(elements.text.isDisplayed(),"Hata mesajı görüntülenemedi.");
 
     }
 }
