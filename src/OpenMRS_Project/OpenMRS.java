2053cf45b663297425cdf731d45791b53f774aed
ZGK <zehragkrl@gmail.com>
2024-04-05 15:18:14 +0300

    US 410 - US 411  tamamlandı.

--- src/OpenMRS_Project/OpenMRS.java
+++ src/OpenMRS_Project/OpenMRS.java
@@ -334,6 +334,37 @@ public class OpenMRS extends BaseDriver {
 
     }
 
+    @Test
+    public void US_410_Zehra() {
+        Zehra_POM elements = new Zehra_POM();
+        elements.login();
+
+        elements.appoinment.click();
+        elements.manageAppoinment.click();
+        wait.until(ExpectedConditions.urlContains("findPatient"));
+        elements.appoinmentSearch.sendKeys("zehra");
+        wait.until(ExpectedConditions.visibilityOf(elements.patientInfo));
+        elements.patientInfo.click();
+        wait.until(ExpectedConditions.urlContains("appointmentschedulingui"));
+        Assert.assertTrue(elements.text.isDisplayed(),"Hata mesajı görüntülenemedi.");
+
+    }
+    @Test
+    public void US_411_Zehra(){
+        Zehra_POM elements = new Zehra_POM();
+        elements.login();
+
+        elements.appoinment.click();
+        elements.manageAppoinment.click();
+        wait.until(ExpectedConditions.urlContains("findPatient"));
+        elements.appoinmentSearch.sendKeys("zehra");
+        wait.until(ExpectedConditions.visibilityOf(elements.patientInfo));
+        elements.patientInfo.click();
+        wait.until(ExpectedConditions.urlContains("appointmentschedulingui"));
+        Assert.assertFalse(elements.text.isDisplayed(),"Hata mesajı görüntülenemedi.");
+
+    }
+
 }
 
 
